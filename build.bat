907799c14a861b2e34741397a046ff61332763f1
mamba <crna.mamba73@gmail.com>
2026-01-13 21:32:58 +0100

    chenged build tool

--- build.bat
+++ build.bat
@@ -1,22 +1,30 @@
 @echo off
+setlocal
+
 set CONFIG=Release
+set MSBUILD="C:\Program Files (x86)\Microsoft Visual Studio\18\BuildTools\MSBuild\Current\Bin\MSBuild.exe"
+set PROJ=mamba.TorchDiscordSync.csproj
 set OUTDIR=bin\%CONFIG%
 set ZIPNAME=mamba.TorchDiscordSync.zip
 
+echo === RESTORE ===
+dotnet restore %PROJ% || goto fail
+
 echo === BUILD ===
-dotnet build mamba.TorchDiscordSync.csproj -c %CONFIG%
-if errorlevel 1 (
-    echo BUILD FAILED
-    exit /b 1
-)
+%MSBUILD% %PROJ% /p:Configuration=%CONFIG% /v:quiet || goto fail
 
 echo === CLEAN ZIP ===
 if exist %ZIPNAME% del %ZIPNAME%
 
 echo === CREATE ZIP ===
-powershell -Command ^
-"Compress-Archive -Path '%OUTDIR%\mamba.TorchDiscordSync.dll','manifest.xml' -DestinationPath '%ZIPNAME%'"
+powershell -Command "Compress-Archive -Path '%OUTDIR%\mamba.TorchDiscordSync.dll','manifest.xml' -DestinationPath '%ZIPNAME%'"
 
 echo === DONE ===
 echo Created: %ZIPNAME%
 pause
+exit /b 0
+
+:fail
+echo BUILD FAILED
+pause
+exit /b 1
